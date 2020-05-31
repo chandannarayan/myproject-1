498562f9f5f71d10ab94beb47d739092a16e6cf7
chandannarayan <55051240+chandannarayan@users.noreply.github.com>
2020-05-31 13:44:54 +0530

    Python class files

---
+++ ClassPractice_20200531.py
@@ -0,0 +1,110 @@
+# sourav@iitp.ac.in
+
+
+a = "Hello, World!"
+print(a[0]) # Always Index starts with '0'
+print(a[3:9])
+
+print(a[-7:])
+
+print(len(a))
+
+'''
+strip() - same as trim in XL
+lower()
+upper()
+replace()
+split()
+if...in
+  
+else...
+
+
+
+
+
+'''
+
+a="Hello"
+b="World"
+c="This is my first Python Program"
+
+d = a + "\n" + b + "\n" + c
+print(d)
+
+
+e = d.replace("\n"," ")
+print(e)
+
+f = e.split(" ")
+print(f)
+
+'''
+tuple - a data type which can include multiple data types. however once defined it cannot be changed
+        Similar to sting and all operations that run on a sting varaible. However, tuple is seperated 
+        by commas
+
+set - A set is a collection  which is unordered and unindexed. In Python sets are written with curly 
+      brackets
+      - Intersection [.intersection or &]
+      - Union [.union or |]
+      - Difference [minus sign -]
+      - Symmetric difference - unique of all the sets
+
+dict - dictionary is a collection of key value pair
+
+'''
+
+mycar = {
+  "brand":"Ford",
+  "model":"Mustand",
+  "year":1964}
+
+print(mycar)
+
+mycar["color"]  = "red"
+
+del thisdict
+
+print(thisdict)
+
+a = ["n","a","f","h","r"]
+
+x= a.sort()
+print(x)
+
+# Ternary Operators
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
